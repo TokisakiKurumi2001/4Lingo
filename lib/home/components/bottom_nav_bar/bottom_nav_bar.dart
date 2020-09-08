74b13afe128a877caa0e8725d0ac8b841b9c9452
GiangHoGoVap <58210406+GiangHoGoVap@users.noreply.github.com>
2020-09-08 18:59:50 +0700

    Add files via upload

---
+++ lib/home/components/bottom_nav_bar/bottom_nav_bar.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:fluttericon/font_awesome5_icons.dart';
+import 'bookmark/bookmark.dart';
+
+class MyBottomNavBar extends StatelessWidget {
+  const MyBottomNavBar({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(
+        left: 40.0,
+        right: 40.0,
+      ),
+      height: 80,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, -10),
+            blurRadius: 35,
+            color: Colors.blue.withOpacity(0.38),
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          IconButton(
+            icon: Icon(
+              FontAwesome5.file_word,
+              color: Colors.blue,
+            ),
+            onPressed: () {
+              //Word popup
+            },
+          ),
+          IconButton(
+            icon: Icon(
+              FontAwesome5.bookmark,
+              color: Colors.blue,
+            ),
+            onPressed: () {
+              //Bookmark popup
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => FlashCard(),
+                ),
+              );
+            },
+          ),
+          IconButton(
+            icon: Icon(
+              FontAwesome5.chart_line,
+              color: Colors.blue,
+            ),
+            onPressed: () {
+              //Statistic popup
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/home/home_page.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:four_lingo/home/components/body.dart';
+import 'package:four_lingo/home/components/bottom_nav_bar/bottom_nav_bar.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: Body(),
+      bottomNavigationBar: MyBottomNavBar(),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: FloatingActionButton(
+          child: Icon(
+            Icons.add,
+          ),
+          onPressed: () {
+            //making flashcard screen popup
+          },
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text(
+                '4Lingo',
+                style: TextStyle(
+                  fontSize: 50.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text(
+                'Guide',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              onTap: () {
+                //Guide popup
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text(
+                'Setting',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              onTap: () {
+                //Setting popup
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      centerTitle: true,
+      elevation: 0,
+      title: Text(
+        '4Lingo',
+        style: TextStyle(
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
