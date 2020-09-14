2d14a382e05d617bf5bea2ce72eef7f525e7c637
DuongNguyen <nhatduong30001@gmail.com>
2020-09-14 23:01:34 +0700

    adding week, month, year and draw grouped charts

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:ForLingo/home/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:ForLingo/home/components/data/week.dart';
+import 'package:ForLingo/home/components/data/month.dart';
+import 'package:ForLingo/home/components/data/year.dart';
 void main() {
   runApp(MyApp());
 }
@@ -8,8 +11,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: HomeScreen(),
+        theme: ThemeData(primarySwatch: Colors.blue),
+        //home: HomeScreen(),
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/', routes: {
+      '/': (context) => HomeScreen(),
+      '/week': (context) => WeekData(),
+      '/month': (context) => MonthData(),
+      '/year': (context) => YearData(),
+    }
     );
   }
 }
