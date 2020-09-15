0b027bdb213a67bb1f75c78ac1c9b3e4e571f92d
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2020-09-15 12:04:24 +0700

    Revert "Fonts & images & charts (#6)" (#7)
    This reverts commit 70916cf37a1bd832dab30f2fb954cf6bc1ec0d19.

--- lib/home/components/bottom_nav_bar/bookmark/words.dart
+++ lib/home/components/bottom_nav_bar/bookmark/words.dart
@@ -1,5 +1,3 @@
-import 'package:charts_flutter/flutter.dart' as dothi;
-import 'package:flutter/material.dart';
 class Words {
   String word;
   String meaning;
@@ -11,53 +9,3 @@ class Words {
     this.sentence = sentence;
   }
 }
-class Day {
-  // Number of vocabs you wish to remember
-  int vocabs;
-  // Number of vocabs you haved remembered
-  int learned;
-  Day({this.learned, this.vocabs});
-}
-class Week {
-  List<Day> myweek = new List(7);
-  int numlearnedvocabs;
-  int totalvocabs;
-  Week(List<Day> temp) {
-    myweek = temp;
-    numlearnedvocabs = 0;
-    totalvocabs = 0;
-    for (int i = 0; i < myweek.length; i++) {
-      numlearnedvocabs += myweek[i].learned;
-      totalvocabs += myweek[i].vocabs;
-    }
-  }
-  Week.fromWeek(int _numlearnedvocab, int _totalvocab)
-  {
-    numlearnedvocabs = _numlearnedvocab;
-    totalvocabs = _totalvocab;
-  }
-}
-
-class Month {
-  List<Week> mymonth = new List(4);
-  int monthtotalvocab;
-  int monthtotallearned;
-  Month(List<Week> temp) {
-    mymonth = temp;
-    monthtotallearned = 0;
-    monthtotalvocab = 0;
-    for (int i = 0; i < mymonth.length; i++) {
-      monthtotallearned += mymonth[i].numlearnedvocabs;
-      monthtotalvocab += mymonth[i].totalvocabs;
-    }
-  }
-}
-// This class is used when we draw a chart
-class ChartData
-{
-  final int numvocabs;
-  final String specifictime; // This can be monday, thurday, august, something,...
-  final dothi.Color barColor;
-  ChartData({ @required this.barColor, @required this.numvocabs, @ required this.specifictime});
-}
-
